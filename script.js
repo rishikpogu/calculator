e46e70890c5b8f44d780c4fac2d7eea2331ee22e
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-06-08 16:21:25 +0530

    define basic operate fn

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ function divide (num1, num2) {
     return num1 / num2;
 }
 
-let firstNum
-let secondNum
+let num1
+let num2
 let operator
+
+function operate (num1, operator, num2) {
+    if (operator == 'add') {
+        add(num1, num2)
+    }
+    else if (operator == 'subtract') {
+        subtract(num1, num2)
+    }
+    else if (operator == 'multiply') {
+        multiply(num1, num2)
+    }
+    else if (operator == 'divide') {
+        divide(num1, num2)
+    }
+}
