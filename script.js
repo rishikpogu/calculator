b327bcf5251f74f3b42feb9f0147b7ff1e096ec0
Rishik <142647228+rishikpogu@users.noreply.github.com>
2024-06-08 20:07:06 +0530

    link all elements, define clear btn, add event listeners for numbers & operators

--- script.js
+++ script.js
@@ -11,24 +11,78 @@ function multiply (num1, num2) {
 }
 
 function divide (num1, num2) {
+    if (num2 === 0) return "ERROR";
     return num1 / num2;
 }
 
-let num1
-let num2
-let operator
+let num1 = ''
+let num2 = ''
+let operator = null
+let result = null
 
-function operate (num1, operator, num2) {
-    if (operator == 'add') {
-        add(num1, num2)
+function operate (operator, num1, num2) {
+    num1 = parseFloat(num1)
+    num2 = parseFloat(num2)
+
+    if (operator == '+') {
+        return add(num1, num2)
     }
-    else if (operator == 'subtract') {
-        subtract(num1, num2)
+    else if (operator == '-') {
+        return subtract(num1, num2)
     }
-    else if (operator == 'multiply') {
-        multiply(num1, num2)
+    else if (operator == '*') {
+        return multiply(num1, num2)
     }
-    else if (operator == 'divide') {
-        divide(num1, num2)
+    else if (operator == '/') {
+        return divide(num1, num2)
     }
 }
+
+const lowerDisplay = document.querySelector('#lower');
+const upperDisplay = document.querySelector('#upper')
+const numbers = document.querySelectorAll('.digit')
+const operators = document.querySelectorAll('.operator');
+const clearButton = document.querySelector('#ac');
+const signButton = document.querySelector('#sign');
+const percentButton = document.querySelector('#percent');
+const equalsButton = document.querySelector('#equals');
+
+clearButton.addEventListener('click', () => {
+    lowerDisplay.textContent = ''
+    upperDisplay.textContent = ''
+    num1 = 0
+    num2 = 0
+    operator  = null
+})
+
+// function updateDisplay() {
+//     upperDisplay.textContent = `${num1} ${operator}`
+//     lowerDisplay.textContent = num2
+// }
+
+numbers.forEach(button => {
+    button.addEventListener('click', () => {
+        lowerDisplay.textContent += button.textContent
+    })
+})
+
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        if (num1 !== '') {
+            num2 = lowerDisplay.textContent
+            if (num2 == '') {
+                return
+            }
+            result = operate(operator, num1, num2)
+            num1 = result
+        }
+        // else if (num1 !== '' && num2 == '') return
+        else {
+            num1 = lowerDisplay.textContent
+        }
+        operator = button.textContent
+        lowerDisplay.textContent = ''
+        if (num1 == '') return
+        else upperDisplay.textContent = `${num1} ${operator}`
+    })
+})
